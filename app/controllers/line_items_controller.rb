0d63a299d1ca863c8ba403d40696deecccce7d52
sa2423 <seerat.aziz15@gmail.com>
2017-07-14 18:22:49 -0400

    Added ajax to cart, javascript for special effects, and channels to allow updated products to appear in cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,7 +31,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-	format.html { redirect_to @line_item.cart }
+	format.html { redirect_to store_index_url }
+	format.js { @current_item = @line_item }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -60,7 +61,7 @@ class LineItemsController < ApplicationController
     @line_item = LineItem.find(params[:id])
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to cart_url(session[:cart_id]), notice: 'Item was removed' }
+      format.html { redirect_to store_index_url }
       format.json { head :no_content }
     end
 
